5556bbb13d104b272123926eae52d34b9b8f43e9
Masoud Ghanbarzadeh <33165677+ghmasood@users.noreply.github.com>
2024-05-01 18:59:59 +0330

    [INIT] React base config

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import '../assets/styles/globals.scss';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '10%' }}>
+      <img src='/logo.svg' width={'400'} />
+    </div>
+  );
+}
+
+export default App;
