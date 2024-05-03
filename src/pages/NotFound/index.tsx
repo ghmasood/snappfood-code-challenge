c839c616185c088eb2252fe7e422dd0da797996a
Masoud Ghanbarzadeh <33165677+ghmasood@users.noreply.github.com>
2024-05-04 00:01:19 +0330

    [ADD] 404 not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+
+import BasicButton from 'components/BasicButton';
+import Brand from 'components/Brand';
+
+import styles from './notFound.module.scss';
+
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.main__oopsContainer}>
+        oops
+        <Brand variant='icon' className={styles.main__icon} />
+      </div>
+      <span className={styles.main__404}>404</span>
+      <span className={styles.main__text}>به نظر می‌آید شما گم شده اید :(</span>
+      <Link to='/'>
+        <BasicButton className={styles.main__button}>برگشت به خانه</BasicButton>
+      </Link>
+    </div>
+  );
 }
 
 export default NotFoundPage;
